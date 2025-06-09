7f63661b131a2237aebb00207c29bcd3c05db703
argan.megariansyah <arganjava@gmail.com>
2025-06-09 23:17:01 +0700

    fix: update metadata URL to a static origin for consistency

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -19,7 +19,7 @@ if (!projectId) {
 const metadata = {
   name: 'appkit-example',
   description: 'AppKit Example',
-  url: window.location.origin, // origin must match your domain & subdomain
+  url: "https://www.tripegi.site", // origin must match your domain & subdomain
   icons: ['https://avatars.githubusercontent.com/u/179229932']
 }
 
